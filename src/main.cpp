f494ba648dd473e0caf0122f85b56e951651c534
KHADRAOUI Ibrahim <khadraouiibrahim@gmail.com>
2021-11-17 16:20:38 +0100

    programm with the new card

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,23 @@
 #include <EEPROM.h>
 
 //Pinout
-#define button1 4
-#define button2 5
-#define button3 6
-#define button4 7
-#define button5 10
-#define button6 11
-#define led1 12
-#define buzzer 13
+#define signal 2
+#define OUTA 3
+#define OUTB 14
+#define BTN 4
+#define buzzer 5
+#define led1 6
+#define led2 7
 #define TX_BT 8
 #define RX_BT 9
+#define button1 10
+#define button2 11
+#define button3 12
+#define button4 14
+#define button5 15
+#define button6 16
 #define debounce_delay 200
-#define LCD_REFRECH 200
+#define LCD_REFRECH 10
 
 //Instances
 LiquidCrystal_I2C lcd(0x27,16,2);
@@ -23,8 +28,12 @@ SoftwareSerial BLE(RX_BT, TX_BT);
 
 //Variables
 bool state, prev_state;
+bool currentDir;
+int currentStateCLK;
+int lastStateCLK;
 int analog_read;
-unsigned long counter = 0;
+volatile unsigned long counter = 0;
+volatile long counterRotary = 0;
 float distance = 0.0, rapport = 988.0;
 bool FWD_BWD = 0;
 bool ETALON = 0;
@@ -42,6 +51,10 @@ void go();
 void setup() {
   pinMode(button1, INPUT);pinMode(button2, INPUT);pinMode(button3, INPUT);pinMode(button4, INPUT);
   pinMode(button5, INPUT);pinMode(button6, INPUT);pinMode(led1, OUTPUT);pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT); pinMode(OUTA, INPUT); pinMode(OUTB, INPUT); pinMode(signal, INPUT);
+  attachInterrupt(digitalPinToInterrupt(OUTA), interruptRotary, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(signal), interruptSignal, CHANGE);
+  lastStateCLK = digitalRead(OUTA);
   BLE.begin(9600);
   BLE.println("CLUB CELEC *** CTTP");
   lcd.init(); lcd.backlight(); lcd.noBlink(); lcd.noCursor();
@@ -58,39 +71,34 @@ void loop() {
   else mainMenu();
 }
 
-/*
-Pour marche avant
-Lire capteur, definir etat du signal (High ou Low), si il y a un 
-chagement d'etat incrementer la variable counter
-*/
-void updateCounterFWD(){
-  analog_read = analogRead(A0);
-  if(analog_read < 50) state = 0;
-  else if(analog_read > 500) state = 1;
-  if(state != prev_state) counter++;
-  prev_state = state;
+void interruptRotary() {
+  currentStateCLK = digitalRead(OUTA);
+	if (currentStateCLK != lastStateCLK  && currentStateCLK == 1){
+		if (digitalRead(OUTB) != currentStateCLK) {
+			counterRotary --;
+			currentDir =true;
+		} 
+    else {
+			counterRotary ++;
+			currentDir =false;
+		}
+  }
 }
 
-/*
-Pour marche avant
-Lire capteur, definir etat du signal (High ou Low), si il y a un 
-chagement d'etat decrementer la variable counter, sans depasser 0
-*/
-void updateCounterBWD(){
-  analog_read = analogRead(A0);
-  if(analog_read < 50) state = 0;
-  else if(analog_read > 500) state = 1;
-  if(state != prev_state && counter > 0) counter--;
-  prev_state = state;
+void interruptSignal() {
+  if (FWD_BWD) {
+    counter++;
+  } else {
+    counter--;
+  } 
+  
 }
 
 /*
 Fonction pour avoir la distance
 eq: dist = counter * 100m / 988
 */
-float get_distance(){
-  if(FWD_BWD == 0) updateCounterFWD();
-  else updateCounterBWD();
+float get_distance() {
   return (counter * 100) / rapport;
 }
 
@@ -132,19 +140,18 @@ int buttonPressed(){
 }
 
 void mainMenu(){
-  //lcd.clear(); 
+  lcd.clear(); 
   lcd.home();
   lcd.print("Distance:");
   lcd.setCursor(9,0);lcd.print(get_distance(), 1);lcd.setCursor(15,0);lcd.print("m");
   lcd.setCursor(0,1);
   lcd.print("A:CL B:Av C:Arr");
-  BLE.print("Distance: ");BLE.println(get_distance());
-  if(FWD_BWD){lcd.setCursor(10,1); lcd.cursor();}
-  else {lcd.setCursor(5,1); lcd.cursor();}
-  delay(LCD_REFRECH);
+  //BLE.print("Distance: ");BLE.println(get_distance());
+  //delay(LCD_REFRECH);
   switch (buttonPressed()){
   case 1:
     counter = 0;
+    lcd.clear();
     break;
   case 2:
     FWD_BWD = 0;
